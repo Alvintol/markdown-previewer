71d8e05f2431b1f96645a09d49da0212b3fb117d
Alvin <alvin.tol@hotmail.com>
2022-06-07 14:22:08 -0600

    combines reducers

--- src/state/editorReducer.js
+++ src/state/editorReducer.js
@@ -1,13 +1,26 @@
-export const editorReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'input': return state + action.payload;
-    default: return state;
-  }
-} 
+import { combineReducers } from 'redux';
 
-export const previewReducer = (state= '', action) => {
+const editorReducer = (state = '', action) => {
   switch (action.type) {
-    case 'preview': return state + action.payload;
-    default: return state;
+    case 'input':
+      return state + action.payload;
+    default:
+      return state;
   }
-}
+};
+
+const previewReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'preview':
+      return state + action.payload;
+    default:
+      return state;
+  }
+};
+
+const reducers = combineReducers({
+  editor: editorReducer,
+  preview: previewReducer,
+});
+
+export default reducers;
