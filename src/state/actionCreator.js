d13b623ce6529517bc8a9d36be622452f31434e1
Alvin <alvin.tol@hotmail.com>
2022-06-08 11:42:05 -0600

    corrects reducer and actionCreators logic

--- src/state/actionCreator.js
+++ src/state/actionCreator.js
@@ -6,25 +6,13 @@ export const updateEditor = (input) => (dispatch) =>
 
 export const maxEditor = (input) => (dispatch) =>
   dispatch({
-    type: 'max',
-    payload: input,
-  });
-
-export const minEditor = (input) => (dispatch) =>
-  dispatch({
-    type: 'min',
+    type: 'editor',
     payload: input,
   });
 
 export const maxPreview = (input) => (dispatch) =>
   dispatch({
-    type: 'max',
-    payload: input,
-  });
-
-export const minPreview = (input) => (dispatch) =>
-  dispatch({
-    type: 'min',
+    type: 'preview',
     payload: input,
   });
 
