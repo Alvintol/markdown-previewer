c50fc277d9c726439a92285fca89dfdcdae7cd2e
Alvin <alvin.tol@hotmail.com>
2022-06-07 20:38:17 -0600

    imports useSelectos, useDispatch & bindActionCreators

--- src/Editor.js
+++ src/Editor.js
@@ -1,21 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import TopBar from './TopBar';
 
-const Editor = props => {
-  
+const Editor = (props) => {
+  // const { editor, preview } = useSelector((state) => state);
+  // const dispatch = useDispatch();
+
+
   const changeEditor = (input) => console.log(input);
 
-return (
-  <div id='mark-down'>
-    <TopBar
-    title='EDITOR'
-    />
-    <textarea
-    id='editor'
-    value={props.editor}
-    onChange={e => changeEditor(e.target.value)}
-    />
-  </div>
-)
+  return (
+    <div id='mark-down'>
+      <TopBar title='EDITOR' />
+      <textarea
+        id='editor'
+        value={props.editor}
+        onChange={(e) => changeEditor(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Editor;
