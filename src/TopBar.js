bdc47f3eb667dab9efa85723884e90e2b707fe11
Alvin <alvin.tol@hotmail.com>
2022-06-08 11:31:13 -0600

    adds min/max/even action creators

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,10 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actionCreator from './state/actionCreator';
+
+
 const TopBar = (props) => {
   const { title } = props;
+
+  const { windowSize } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { changeWindow } = bindActionCreators(actionCreator, dispatch);
+
   return (
     <div className='top-bar'>
       {title}
       <div className='top-container'>
-        <button><i class="fa-solid fa-maximize"></i></button>
+        <button>
+          <i className='fa-solid fa-maximize'></i>
+        </button>
       </div>
     </div>
   );
--- src/state/actionCreator.js
+++ src/state/actionCreator.js
@@ -1,8 +1,35 @@
-export const updateEditor = (input) => {
-  return (dispatch) => {
-    dispatch({
-      type: 'input',
-      payload: input
-    });
-  };
-};
+export const updateEditor = (input) => (dispatch) =>
+  dispatch({
+    type: 'input',
+    payload: input,
+  });
+
+export const maxEditor = (input) => (dispatch) =>
+  dispatch({
+    type: 'max',
+    payload: input,
+  });
+
+export const minEditor = (input) => (dispatch) =>
+  dispatch({
+    type: 'min',
+    payload: input,
+  });
+
+export const maxPreview = (input) => (dispatch) =>
+  dispatch({
+    type: 'max',
+    payload: input,
+  });
+
+export const minPreview = (input) => (dispatch) =>
+  dispatch({
+    type: 'min',
+    payload: input,
+  });
+
+export const evenView = (input) => (dispatch) =>
+  dispatch({
+    type: 'even',
+    payload: input,
+  });
