bd2ded9b5fb20a880e317a1dbd17fb77e5f389dd
Alvin <alvin.tol@hotmail.com>
2022-06-08 15:10:56 -0600

    adds helper function to toggle editor page size

--- src/Editor.js
+++ src/Editor.js
@@ -1,16 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { toggleWindowSize } from './helper/helper_functions';
 import * as actionCreator from './state/actionCreator';
 import TopBar from './TopBar';
 
 const Editor = (props) => {
-  const { editor } = useSelector((state) => state);
+
+  const { editor, windowSize } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const { updateEditor } = bindActionCreators(actionCreator, dispatch);
 
+  const window = windowSize === 'PREVIEW' ? {display: 'none'} : toggleWindowSize(windowSize);
+
   return (
-    <div id='mark-down'>
+    <div id='mark-down' style={window}>
       <TopBar title='EDITOR' />
       <textarea
         id='editor'
---
+++ src/helper/helper_functions.js
@@ -0,0 +1,2 @@
+export const toggleWindowSize = (windowSize) => 
+windowSize === 'EDITOR' ? {height: '90vh'} : null;
